c2cb1f0bef853a35606870b0c86be128759b01dd
Tzuzul Code <mail@tzuzulcode.com>
2022-02-04 08:06:45 -0600

    Passport GoogleOauthStrategy

--- graphql/src/index.js
+++ graphql/src/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const cors = require("cors")
 const cookie = require("cookie-parser")
+const passport = require("passport")
+const googleStrategy = require("passport-google-oauth")
+const GoogleStrategy = googleStrategy.OAuth2Strategy
 //DB
 const {connection} = require("./config/db")
 connection()
@@ -14,6 +17,8 @@ const auth = require('./routes/auth')
 
 const app = express()
 
+
+//Middleware
 // Agregando middleware para body parsing
 app.use(express.json())
 app.use(cors({
@@ -21,6 +26,18 @@ app.use(cors({
     credentials:true
 }))
 app.use(cookie())
+app.use(passport.initialize())
+
+passport.use(new GoogleStrategy({
+    clientID:"982539754927-1hvt50qhhbmsrivgleq3m6gstbp5na4c.apps.googleusercontent.com",
+    clientSecret:"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",
+    callbackURL:"http://localhost:8000/google/callback",
+    passReqToCallback:true
+},(req,accessToken, refreshToken, profile, cb)=>{
+    console.log(accessToken)
+    console.log("REQUEST",req)
+    cb(null,{profile,accessToken})
+}))
 
 graphql(app)
 users(app)
@@ -28,7 +45,18 @@ products(app)
 apollo(app)
 auth(app)
 
-//Middleware
+
+//Ruta de ejemplo para passport
+app.get("/google",passport.authenticate("google",{
+    scope:['email','profile']
+}))
+
+app.get("/google/callback",passport.authenticate("google",(error,details)=>{
+    console.log(error)
+    console.log(details)
+}),(req,res)=>{
+    return res.redirect("/")
+})
 
 
 app.get('/',(req,res)=>{
